4eafe78cc7578d301a9ef38a30e750af033f157e
SayyedAquib <sayyed.aquib89@gmail.com>
2025-01-02 00:23:15 +0530

    Add cookie-parser and cors middleware; implement error handling and response classes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: "./env"
+  path: "./env",
 });
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Mongo db connection failed!!! ", error);
+  });
 
 /*
 import mongoose from "mongoose";
