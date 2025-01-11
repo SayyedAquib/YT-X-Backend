7519951032fb67ffabc9d064c1aaa907db577b04
SayyedAquib <sayyed.aquib89@gmail.com>
2025-01-11 14:37:23 +0530

    Refactor logoutUser to use $unset for refreshToken and improve formatting in getWatchHistory response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -164,8 +164,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
@@ -459,7 +459,13 @@ const getWatchHistory = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-  .json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"));
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully"
+      )
+    );
 });
 
 export {
